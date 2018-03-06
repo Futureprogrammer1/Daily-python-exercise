342affe696ed63fe91bc0ddab2b1fc7fb454157f
Futureprogrammer1 <33432524+Futureprogrammer1@users.noreply.github.com>
2018-03-06 13:42:33 -0700

    Add files via upload

---
+++ Python exercise 2.py
@@ -0,0 +1,7 @@
+#Bryan Ventura Ortiz - Chapter 2 - Exercise 2 
+#Enter your name and print
+name = str(input('Enter your name: '))
+print ('Hello ' + name )
+Age= int(input("Enter your age:" ))
+x = Age - 100
+print ("you will be one hundrerd in " + str(x) + "Years" )
